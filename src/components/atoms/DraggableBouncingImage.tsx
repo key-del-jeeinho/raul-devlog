ee1b27c086a32d37f1eca40e2013b1c37ed1ff7d
raul <develop.raul@gmail.com>
2023-05-04 06:13:42 +0900

    add: DraggableBouncingImage

---
+++ src/components/atoms/DraggableBouncingImage.tsx
@@ -0,0 +1,19 @@
+import { ImageProps } from "next/image"
+import BouncingImage from "./BouncingImage"
+import DraggableComponent from "./DraggableComponent"
+
+interface Props {
+    isFixed: boolean,
+    src: string,
+    alt: string,
+    width: number | null,
+    height: number | null
+}
+
+export default function DraggableBouncingImage({src, alt, isFixed, width, height}: Props) {
+    return (
+        <DraggableComponent isFixed={isFixed}>
+            <BouncingImage src={src} alt={alt} width={width} height={height}/>
+        </DraggableComponent>
+    )
+}
