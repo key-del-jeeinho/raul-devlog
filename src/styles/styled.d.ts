d8d93064110ac449058cafb6718f3d7273576b50
raul <develop.raul@gmail.com>
2023-05-04 09:06:39 +0900

    fix: theme

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -3,7 +3,27 @@ import "styled-components"
 declare module "styled-components" {
     export interface DefaultTheme {
         colors: {
-            titleText: string;
-        }
+            text_fill__label_colored: string,
+            text_fill__label_texture: string,
+            text_fill__label_monotone: string,
+            text_fill__label_frame: string,
+
+            text_outline__label_frame: string,
+
+            box_outline__label_frame: string,
+            
+            box_fill__label_colored: string,
+            box_fill__label_frame: string,
+            box_fill__label_monotone: string,
+        },
+        font_weights: {
+            label: string,
+        },
+        font_sizes: {
+            label: string
+        },
+        textures: {
+            box_texture__label_texture: string
+        },
     }
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,8 +1,36 @@
 import { DefaultTheme } from "styled-components";
+import paper002 from '../../public/images/img_paper-002.png'
+
+const primary = "#1B5E20"
+const black = "#000000"
+const neutral2 = "#424242"
+const neutral3 = "#9E9E9E"
+const neutral4 = "#E0E0E0"
+const white = "#FFFFFF"
 
 export const lightTheme: DefaultTheme = {
     colors: {
-        titleText: "#000000"
-    }
+        text_fill__label_texture: black,
+
+        text_fill__label_colored: primary,
+        box_fill__label_colored: white,
+
+        text_outline__label_frame: black,
+        box_outline__label_frame: black,
+        box_fill__label_frame: white,
+        text_fill__label_frame: white,
+
+        text_fill__label_monotone: white,
+        box_fill__label_monotone: black,
+    },
+    font_weights: {
+        label: "bold",
+    },
+    font_sizes: {
+        label: "24pt"
+    },
+    textures: {
+        box_texture__label_texture: paper002.src
+    },
 }
 export const defaultTheme: DefaultTheme = lightTheme
