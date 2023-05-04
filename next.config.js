d4a03ca8ace39c224f47994a5e5b8e119749b3df
raul <develop.raul@gmail.com>
2023-05-04 09:13:38 +0900

    fix: className error

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['source.unsplash.com']
-  }
+  },
+  compiler: {
+    styledComponents: true,
+  },
 }
 
 module.exports = nextConfig
