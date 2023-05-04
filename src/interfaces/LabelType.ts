0863168b5af0a95ad025ae4477f6e1a49465c247
raul <develop.raul@gmail.com>
2023-05-04 13:13:10 +0900

    add: randomize LabelType logic

--- src/interfaces/LabelType.ts
+++ src/interfaces/LabelType.ts
@@ -1,2 +1,8 @@
 type LabelType = 'colored'|'frame'|'texture'|'monotone'|'random'
 export default LabelType
+
+export function getRandomLabelType(): LabelType {
+    const labelTypes: LabelType[] = ['colored', 'frame', 'texture', 'monotone'];
+    const randomIndex: number = Math.floor(Math.random() * labelTypes.length);
+    return labelTypes[randomIndex];
+}
