c8dedb0759ae23662d6d7d4ca9aa31197f936c86
raul <develop.raul@gmail.com>
2023-05-05 05:12:29 +0900

    remove: unused import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import '@/styles/globals.css'
 import { lightTheme } from '@/styles/theme'
 import withTheme from '@/styles/withTheme'
 import type { AppProps } from 'next/app'
-import { ThemeProvider } from 'styled-components'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
