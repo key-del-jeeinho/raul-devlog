44de103437798a954717705e08b88e94ff359e1b
raul <develop.raul@gmail.com>
2023-05-05 17:41:54 +0900

    add: theme (for Button)

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -6,6 +6,7 @@ declare module "styled-components" {
             text_fill__title: string,
             text_fill__sub_title: string,
             text_fill__primary_button: string,
+            text_fill__button: string,
             text_fill__label_colored: string,
             text_fill__label_texture: string,
             text_fill__label_monotone: string,
@@ -13,32 +14,43 @@ declare module "styled-components" {
 
             text_outline__label_frame: string,
 
+            box_outline__button: string,
             box_outline__label_frame: string,
             box_outline__label_colored: string,
             box_outline__label_texture: string,
             box_outline__label_monotone: string,
 
             box_fill__primary_button: string,
+            box_fill__button: string,
             box_fill__label_colored: string,
             box_fill__label_frame: string,
             box_fill__label_monotone: string,
+
+            box_shadow__primary_button: string,
+            box_shadow__button: string,
         },
         font_weights: {
             label: string,
             title: string,
-            sub_title: string
+            sub_title: string,
+            primary_button: string,
+            button: string,
         },
         font_sizes: {
-            label: string
-            title: string
-            sub_title: string
+            label: string,
+            title: string,
+            sub_title: string,
+            primary_button: string,
+            button: string,
         },
         font_families: {
-            title: string
-            sub_title: string
+            title: string,
+            sub_title: string,
+            primary_button: string,
+            button: string,
         },
         textures: {
-            box_texture__label_texture: string
+            box_texture__label_texture: string,
         },
     }
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,6 +2,7 @@ import { DefaultTheme } from "styled-components";
 import paper002 from '../../public/images/img_paper-002.png'
 
 const primary = "#1B5E20"
+const primary_light = "#206D1F"
 const black = "#000000"
 const neutral2 = "#424242"
 const neutral3 = "#9E9E9E"
@@ -15,6 +16,12 @@ export const lightTheme: DefaultTheme = {
 
         text_fill__primary_button: white,
         box_fill__primary_button: primary,
+        box_shadow__primary_button: black,
+
+        text_fill__button: black,
+        box_outline__button: primary,
+        box_fill__button: white,
+        box_shadow__button: primary_light,
 
         text_fill__label_texture: black,
         box_outline__label_texture: black,
@@ -35,16 +42,22 @@ export const lightTheme: DefaultTheme = {
     font_weights: {
         label: "bold",
         title: "bold",
-        sub_title: "regular"
+        sub_title: "regular",
+        primary_button: "bold",
+        button: "bold",
     },
     font_sizes: {
         label: "24pt",
         title: "96pt",
-        sub_title: "24pt"
+        sub_title: "24pt",
+        primary_button: "24pt",
+        button: "16pt",
     },
     font_families: {
         title: "PyeongChang",
         sub_title: "Pretendard",
+        primary_button: "Pretendard",
+        button: "Pretendard",
     },
     textures: {
         box_texture__label_texture: paper002.src,
