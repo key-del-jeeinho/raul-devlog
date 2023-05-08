54c57fce45a4be67e3a28b67686f11ecd0dbb06b
지인호 [Raul] <raul@daangn.com>
2023-05-09 08:51:32 +0900

    fix: _app.ts
    - remove unnecessary function 'withTheme'
    - add recoil root
    - add theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,19 @@
 import GlobalStyle from '@/styles/GlobalStyle'
 import '@/styles/globals.css'
-import { lightTheme } from '@/styles/theme'
-import withTheme from '@/styles/withTheme'
+import { defaultTheme, lightTheme } from '@/styles/theme'
 import type { AppProps } from 'next/app'
+import { RecoilRoot } from 'recoil'
+import { ThemeProvider } from 'styled-components'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>{
-      withTheme(
-          <>
+    <>
+      <RecoilRoot>
+        <ThemeProvider theme={defaultTheme}>
             <GlobalStyle />
             <Component {...pageProps} />
-          </>
-      )
-    }</>
+        </ThemeProvider>
+      </RecoilRoot>
+    </>
   )
 }
