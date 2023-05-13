373a7ce202d6410b43ddb7279c1efed8c98f18ea
지인호 [Raul] <raul@daangn.com>
2023-05-13 15:04:03 +0900

    remove: a tag default style

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -3,6 +3,13 @@ import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
 ${reset}
+a { 
+    text-decoration:none;
+    color: inherit;
+    :hover { 
+        text-decoration:none
+    }
+}
 * {
     box-sizing: border-box;
     padding: 0;
