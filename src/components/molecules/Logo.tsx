5529a56ddb2f532c547472f91933a3ef50e36c31
지인호 [Raul] <raul@daangn.com>
2023-05-13 14:55:24 +0900

    fix: Logo style (z-index and position)

--- src/components/molecules/Logo.tsx
+++ src/components/molecules/Logo.tsx
@@ -1,7 +1,13 @@
-import styled from "styled-components"
 import BouncingImage from "./BouncingImage"
 import logoImage from "../../../public/images/img_logo.png"
 
+const Z_INDEX_MAX = 2147483647
 export default function Logo() {
-    return (<BouncingImage src={logoImage} alt="logo" width={162} height={184}/>)
+    return (<span style={{zIndex: Z_INDEX_MAX, position: "relative"}}>
+                <BouncingImage 
+                    src={logoImage}
+                    alt="logo" width={162}
+                    height={184}
+                />
+            </span>)
 }
