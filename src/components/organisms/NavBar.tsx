98b383039eb006899ab189da62b9de6cb5594893
지인호 [Raul] <raul@daangn.com>
2023-05-13 15:28:26 +0900

    fix: NavBar (refactor)

--- src/components/organisms/NavBar.tsx
+++ src/components/organisms/NavBar.tsx
@@ -4,20 +4,23 @@ import Icon from "../atoms/Icon";
 import { ReactNode, useContext } from "react";
 import Direction from "../../interfaces/Direction";
 import { createContext } from "react";
+import Link from "next/link";
 
 type NavBarContextProp = {
     direction: Direction,
 }
+
 const NavBarContext = createContext<NavBarContextProp | null>(null)
 
+const navBarHeightPx_110 = 110
 const NavBarStyle = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-between;
     width: 100vw;
-    height: 110px;
-    background-color: white;
-    border-bottom: 1px solid black;
+    height: ${navBarHeightPx_110}px;
+    background-color: ${({theme}) => theme.colors.box_fill__nav_bar};
+    border-bottom: 2px solid ${({theme}) => theme.colors.box_outline__nav_bar};
 `
 
 interface Props {
@@ -30,11 +33,11 @@ export default function NavBar({}: Props) {
     return (
         <NavBarStyle>
             <NavBar.Left>
-                <NavBar.Logo/>
+                <Link href="/"><NavBar.Logo/></Link>
             </NavBar.Left>
             <NavBar.Right>
-                <NavBar.Attribute>About.me</NavBar.Attribute>
-                <NavBar.Attribute>About.me</NavBar.Attribute>
+                <NavBar.Attribute><Link href="/about-me">About.me</Link></NavBar.Attribute>
+                <NavBar.Attribute><Link href="/dev-post">Dev.post</Link></NavBar.Attribute>
                 <NavBar.Attribute isLast={true}><Icon type='ic32_menu' color={iconColor} /></NavBar.Attribute>
             </NavBar.Right>
         </NavBarStyle>
@@ -57,7 +60,7 @@ const NavBarLeftStyle = styled.div`
     display: flex;
     flex-direction: row;
     align-items: center;
-    padding-right: 55px;
+    padding-right: ${navBarHeightPx_110 / 2}px;
 `
 
 NavBar.Left = function NavBarLeft({children}: NavBarComponentProps): JSX.Element {
@@ -74,8 +77,9 @@ const NavBarRightStyle = styled.div`
     display: flex;
     flex-direction: row;
     align-items: center;
-    padding-right: 55px;
+    padding-right: ${navBarHeightPx_110 / 2}px;
 `
+
 NavBar.Right = function NavBarRight({children}: NavBarComponentProps): JSX.Element {
     const direction: Direction = "Right"
     const providerValue: NavBarContextProp = { direction }
@@ -98,10 +102,10 @@ const NavBarAttributeStyle = styled.div<NavBarAttributeStyleProp>`
     ${({isLast, direction}) => {
         if(isLast) {
             switch(direction) {
-                case "Right": return "padding-left: 3.5vw"
-                case "Left": return "padding-right: 3.5vw"
+                case "Right": return "padding-left: 2.5vw"
+                case "Left": return "padding-right: 2.5vw"
             }
-        } else return "padding: 0 3.5vw"
+        } else return "padding: 0 2.5vw"
     }}
 `
 
