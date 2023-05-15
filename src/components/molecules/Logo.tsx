7dd63472e9a99632673b14125c9bc576cf61a70c
지인호 [Raul] <raul@daangn.com>
2023-05-16 08:21:40 +0900

    fix: Logo (dark mode support)

--- src/components/molecules/Logo.tsx
+++ src/components/molecules/Logo.tsx
@@ -1,13 +1,25 @@
 import BouncingImage from "./BouncingImage"
-import logoImage from "../../../public/images/img_logo.png"
+import logoImage_light from "../../../public/images/img_logo.png"
+import logoImage_dark from "../../../public/images/img_logo-dark.png"
+import useBlogTheme from "@/hooks/useBlogTheme"
+import BlogTheme from "@/interfaces/BlogTheme"
 
 const Z_INDEX_MAX = 2147483647
 export default function Logo() {
+    const [theme] = useBlogTheme()
+    const image = getLogoImageByTheme(theme)
     return (<span style={{zIndex: Z_INDEX_MAX, position: "relative"}}>
                 <BouncingImage 
-                    src={logoImage}
+                    src={image}
                     alt="logo" width={162}
                     height={184}
                 />
             </span>)
+}
+
+function getLogoImageByTheme(theme: BlogTheme) {
+    switch(theme) {
+        case "light": return logoImage_light
+        case "dark": return logoImage_dark
+    }
 }
